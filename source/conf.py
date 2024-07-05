1746ca99350ecb226c39a728f625c2a24277f825
Pushkar <121485926+dpshkr@users.noreply.github.com>
2024-07-06 03:47:01 +0530

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,45 +1,28 @@
-# -*- coding: utf-8 -*-
-# Note that not all possible configuration values are present. All configuration
-# values have a default.
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- General configuration --
-project = '2D Semiconductors'
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Physics of 2D Semiconductors'
 copyright = '2024, Pushkar'
 author = 'Pushkar'
+release = '0.1'
 
-master_doc = 'index'
-version = 'latest'
-pygments_style = 'sphinx'
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
- "sphinxext.opengraph",
- "notfound.extension",
- "sphinx_sitemap"
-]
+extensions = []
 
-sitemap_filename = "sitemap.xml"
+templates_path = ['_templates']
+exclude_patterns = []
 
-html_css_files = ['custom.css']
-html_js_files = ['custom.js']
-html_theme = 'sphinx_rtd_theme'
-html_baseurl = 'https://sphinx-netlify.netlify.app/'
-html_theme_options = {
-    'current_version': version,
-    "collapse_navigation": False,
-}
-html_context = {
-  'display_github': True,
-  'github_user': 'dpshkr',
-  'github_repo': 'science-book',
-  'github_version': 'main/source/'
-}
 
-html_favicon = '_static/favicon.ico'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
 html_static_path = ['_static']
-html_extra_path = ['robots.txt']
-
-# If true, links to the reST sources are added to the pages.
-html_show_sourcelink = True
-html_show_copyright = True
-
-htmlhelp_basename = 'ContinuousSphinxDoc'
